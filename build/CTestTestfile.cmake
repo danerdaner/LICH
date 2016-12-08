bc47dcdbc0d8a855e95eb9058dbf91d4b42b4232
Frank Gaede <frank.gaede@desy.de>
2016-12-08 10:58:17 +0100

    remove ./build dir and *~

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/ihep.ac.cn/users/y/yudan/yudan/proPID
-# Build directory: /afs/ihep.ac.cn/users/y/yudan/yudan/proPID/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
